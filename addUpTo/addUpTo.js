733fe93592dd07a04785e5977089938e2a57d37e
Mohfez Rahman <mohfezrahman@gmail.com>
2021-12-01 09:49:14 -0500

    updated

--- addUpTo/addUpTo.js
+++ addUpTo/addUpTo.js
@@ -1,8 +1,12 @@
-function addUpTo(n){
-    var total=0
-    for(var i=0;i<=n;i++){
-        total+=i
-    }
-    return total
+function addUpTo(n) {
+  var total = 0;
+  for (var i = 0; i <= n; i++) {
+    total += i;
+  }
+  return total;
 }
-console.log(addUpTo(3))
+var t1 = performance.now();
+
+var t2 = performance.now();
+console.log(addUpTo(3));
+console.log("Time Elapsed:" + (t1 - t2) / 1000 + "seconds.");
--- addUpTo/addUpTo2.js
+++ addUpTo/addUpTo2.js
@@ -1,4 +1,8 @@
-function addUpTo(n){
-    return n*(n+1)/2
+function addUpTo(n) {
+  return (n * (n + 1)) / 2;
 }
-console.log(addUpTo(3))
+var t1 = performance.now();
+
+var t2 = performance.now();
+console.log(addUpTo(3));
+console.log("Time Elapsed:" + (t1 - t2) / 1000 + "seconds.");
